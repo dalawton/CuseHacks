6fa229977069d40c7db19e644699bd8f70a267c6
alantomw <145681461+alantomw@users.noreply.github.com>
2023-12-02 15:13:31 -0500

    Create colors.py
    color setup

---
+++ colors.py
@@ -0,0 +1,14 @@
+import pygame
+
+# colors 
+white = pygame.Color(255, 255, 255, 255)
+black = pygame.Color(0, 0, 0, 255)
+seagreen = pygame.Color(46, 139, 87, 255)
+seagreen1 = pygame.Color(84, 255, 159, 255)
+seagreen2 = pygame.Color(78, 238, 148, 255)
+seagreen3 = pygame.Color(67, 205, 128, 255)
+palegreen = pygame.Color(152, 251, 152, 255)
+palegreen1 = pygame.Color(154, 255, 154, 255)
+palegreen2 = pygame.Color(144, 238, 144, 255)
+palegreen3 = pygame.Color(124, 205, 124, 255)
+palegreen4 = pygame.Color(84, 139, 84, 255)
